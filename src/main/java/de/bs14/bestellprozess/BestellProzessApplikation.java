f1fbb7e6fd31ea999be934668dd2d3022468e541
mansour <mansour.kohestani@yahoo.de>
2018-11-20 22:40:25 +0100

    test

---
+++ src/main/java/de/bs14/bestellprozess/BestellProzessApplikation.java
@@ -0,0 +1,12 @@
+package de.bs14.bestellprozess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestellProzessApplikation {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestellProzessApplikation.class, args);
+    }
+}
